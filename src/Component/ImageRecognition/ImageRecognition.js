ef8958ff7a723e93a80f074aaa630d97e83d00ad
sami-svg <samisuls9@gmail.com>
2020-02-12 20:51:48 +0600

    update style imagerecognition

--- src/Component/ImageRecognition/ImageRecognition.js
+++ src/Component/ImageRecognition/ImageRecognition.js
@@ -7,7 +7,7 @@ function ImageRecognition({onButtonChange,input}) {
 			</p>
 	    <div className="ImageRecognition f2 pa4 center">
 	      <input onChange={input} className=" w-70" type="text"/>
-	      <button onClick={onButtonChange} className="w-30 pointer black hover-white bg-gray">Detect</button>
+	      <button onClick={onButtonChange} className="w-30 pointer black dib hover-white bg-gray">Detect</button>
 	    </div>
     </div>  
   );
