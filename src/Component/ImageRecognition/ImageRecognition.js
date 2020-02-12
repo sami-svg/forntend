58862e29b73217fe83f126e88709efd3bf51ccb6
sami-svg <samisuls9@gmail.com>
2020-02-12 21:10:14 +0600

    update imagerecognition

--- src/Component/ImageRecognition/ImageRecognition.js
+++ src/Component/ImageRecognition/ImageRecognition.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './ImageRecognition.css';
 function ImageRecognition({onButtonChange,input}) {
   return (
+
   		 <div className="tc">
-	 		<p className="f4 white">{`This Magic Brain will detect Faces.Give it try... `}
-			</p>
-	    <div className="ImageRecognition f2 pa4 center">
-	      <input onChange={input} className=" w-70" type="text"/>
-	      <button onClick={onButtonChange} className="w-30 pointer black dib hover-white bg-gray">Detect</button>
-	    </div>
-    </div>  
+		 		<p className="f4 white">{`This Magic Brain will detect Faces.Give it try... `}
+				</p>
+			    <div className="ImageRecognition f2 pa4 center shadow-5">
+			      <input onChange={input} className="center  w-70" type="text"/>
+			      <button onClick={onButtonChange} className="w-30 pointer black dib hover-white bg-gray">Detect</button>
+			    </div>
+   		 </div>
   );
 }
 
