6e16b77cc3304453955d2089863c2fdb5e8625d1
sami-svg <samisuls9@gmail.com>
2020-02-13 11:50:09 +0600

    add css

--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -4,15 +4,15 @@ function Navigation({isSignedIn,route}) {
 if(!isSignedIn){
 	return (
     	<nav className="f3 mb6 mr4 pv2  white">
-    		<p onClick={()=>route("signout")} className="ml3 fr no-underline hover-dark-green underline-hover pointer dim ">SignIn</p>
-    		<p onClick={()=>route("register")} className="fr pointer no-underline hover-dark-green underline-hover dim">Register</p>
+    		<p onClick={()=>route("signout")} className="ml3 fr no-underline hover-dark-green underline-hover pointer">SignIn</p>
+    		<p onClick={()=>route("register")} className="fr pointer no-underline hover-dark-green underline-hover">Register</p>
     	</nav>
  	);
 }
 else{
 	return (
     	<nav className="f3 mr4 pv2  white">
-    		<p onClick={()=>route("signout")} className="ml3 fr no-underline underline-hover pointer dim ">SignOut</p>
+    		<p onClick={()=>route("signout")} className="ml3 fr hover-dark-green no-underline underline-hover pointer">SignOut</p>
     	</nav>
   		);	
 	}
