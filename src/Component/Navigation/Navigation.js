fd603ee7da1cdc3f1c2ae432e135eb61927e32c2
sami-svg <samisuls9@gmail.com>
2020-02-13 11:55:31 +0600

    add css colr

--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -3,16 +3,16 @@ import React from 'react';
 function Navigation({isSignedIn,route}) {
 if(!isSignedIn){
 	return (
-    	<nav className="f3 mb6 mr4 pv2  white">
-    		<p onClick={()=>route("signout")} className="ml3 fr no-underline hover-dark-green underline-hover pointer">SignIn</p>
-    		<p onClick={()=>route("register")} className="fr pointer no-underline hover-dark-green underline-hover">Register</p>
+    	<nav className="f3 mb6 mr4 pv2 black">
+    		<p onClick={()=>route("signout")} className="ml3 fr no-underline hover-white underline-hover pointer">SignIn</p>
+    		<p onClick={()=>route("register")} className="fr pointer no-underline hover-white underline-hover">Register</p>
     	</nav>
  	);
 }
 else{
 	return (
-    	<nav className="f3 mr4 pv2  white">
-    		<p onClick={()=>route("signout")} className="ml3 fr hover-dark-green no-underline underline-hover pointer">SignOut</p>
+    	<nav className="f3 mr4 pv2  black">
+    		<p onClick={()=>route("signout")} className="ml3 fr hover-white no-underline underline-hover pointer">SignOut</p>
     	</nav>
   		);	
 	}
