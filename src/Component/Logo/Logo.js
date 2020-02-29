1d3b5680dfee0adc83b5674eb7af19e2ad9b7ef0
sami-svg <samisuls9@gmail.com>
2020-02-29 20:27:53 +0600

    remove pv

--- src/Component/Logo/Logo.js
+++ src/Component/Logo/Logo.js
@@ -5,7 +5,7 @@ import logo from './brain-min.gif';
 
 function Logo() {
   return (
-    <div className="ml5 mt4 pv4 Logo">
+    <div className="ml5 mt4  Logo">
    		<Tilt className="Tilt bw2 shadow-5" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
 		 <div className="Tilt-inner"><img src={logo} alt=""/> </div>
 		</Tilt>
